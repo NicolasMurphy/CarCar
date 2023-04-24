58586774505f7d7f61d183853343428773d22640
Nicolas Murphy <murphynick10@gmail.com>
2023-04-24 17:52:45 -0500

    poller working?? started on technicians

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_id = models.PositiveSmallIntegerField()
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=100)
+    status = models.CharField(max_length=10)
+    vin = models.CharField(max_length=17, unique=True)
+    customer = models.CharField(max_length=100)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.CASCADE,
+    )
+
+
+# maybe status like this?
+    # status = models.ForeignKey(
+    #     Status,
+    #     related_name="presentations",
+    #     on_delete=models.PROTECT,
+    # )
+
+    # def approve(self):
+    #     status = Status.objects.get(name="APPROVED")
+    #     self.status = status
+    #     self.save()
+
+    # def reject(self):
+    #     status = Status.objects.get(name="REJECTED")
+    #     self.status = status
+    #     self.save()
+
+    # @classmethod
+    # def create(cls, **kwargs):
+    #     kwargs["status"] = Status.objects.get(name="SUBMITTED")
+    #     presentation = cls(**kwargs)
+    #     presentation.save()
+    #     return presentation
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from common.json import ModelEncoder
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from .models import Technician
 
-# Create your views here.
+
+class TechnicianListEncoder(ModelEncoder):
+    model = Technician
+    properties = ["first_name", "last_name", "employee_id"]
+
+
+@require_http_methods(["GET"])
+def api_list_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianListEncoder,
+            safe=False,
+        )

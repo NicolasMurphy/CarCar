317efd37ecbc933fb48ff7d4bc119c47ba48f0dc
Nicolas Murphy <murphynick10@gmail.com>
2023-05-01 15:40:51 -0500

    carousel working but broke everything else

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -2,7 +2,7 @@ function MainPage() {
   return (
 
     <>
-
+    {/* <br></br>
     <div className="bg-blue-700 text-center rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl w-full">
       <h1 className="font-bold text-white">CarCar</h1>
       <div className="">
@@ -11,45 +11,31 @@ function MainPage() {
           management!
         </p>
       </div>
+    </div> */}
+
+<div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="https://www.hdcarwallpapers.com/walls/aston_martin_dbs_770_ultimate_2023_8k_3-HD.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a>
+      <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
-
-{/* <div id="default-carousel" className="relative" data-carousel="static">
-
-<div className="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-
-    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-        <span className="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-        <img src="https://www.hdcarwallpapers.com/walls/aston_martin_dbs_770_ultimate_2023_8k_3-HD.jpg" className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
+  </div>
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="https://wallpapercave.com/wp/wp5636803.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a>
+      <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
-
-    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="https://images.hdqwalls.com/wallpapers/ford-gt-2020-42.jpg" className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
+  </div>
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="https://images.hdqwalls.com/wallpapers/ford-gt-2020-42.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a>
+      <a href="#slide1" className="btn btn-circle">❯</a>
     </div>
-
-    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="https://wallpapercave.com/wp/wp5636803.jpg" className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
-    </div>
+  </div>
 </div>
-
-<div className="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-</div>
-
-<button type="button" className="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-    <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <svg className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-        <span className="hidden">Previous</span>
-    </span>
-</button>
-<button type="button" className="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
-    <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <svg className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-        <span className="hidden">Next</span>
-    </span>
-</button>
-</div> */}
 
 </>
 

dedaf9250a99dc5fd03bfbbfa767190872256b48
Paul <paulbaumann830@gmail.com>
2023-05-02 10:02:51 -0500

    fixed the navbar

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -2,101 +2,54 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-      <nav className="bg-green-500">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between h-16">
-            <div className="flex-shrink-0 flex items-center">
-              <NavLink className="text-white font-bold text-xl" to="/">CarCar</NavLink>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
-              <div className="relative">
-                <button className="bg-green-500 text-black font-semibold py-2 px-4 rounded inline-flex items-center nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                  <span className="px-2">Inventory</span>
-                </button>
-                <div className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/manufacturers">
-                        <span className="ml-4 text-base font-medium text-gray-900">Manufacturers</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/manufacturers/new">
-                        <span className="ml-4 text-base font-medium text-gray-900">Add a Manufacturer</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/models">
-                        <span className="ml-4 text-base font-medium text-gray-900">Models</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/models/new">
-                        <span className="ml-4 text-base font-medium text-gray-900">Add a Model</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/automobiles">
-                        <span className="ml-4 text-base font-medium text-gray-900">Automobiles</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/automobiles/new">
-                        <span className="ml-4 text-base font-medium text-gray-900">Add an Automobile</span>
-                      </NavLink>
-                    </div>
-                  </div>
+    <nav className="bg-green-500">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16">
+          <div className="flex-shrink-0 flex items-center">
+            <NavLink className="text-white font-bold text-xl" to="/">CarCar</NavLink>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-8 flex items-baseline my-3 space-x-2">
+
+              <button id="dropdownDefaultButton1" data-dropdown-toggle="dropdown1" className="text-white bg-grey-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Inventory <svg className="w-4 h-6 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                <div id="dropdown1" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton1">
+                    <li>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/manufacturers">Manufacturers</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/manufacturers/new">Add a Manufacturer</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/models">Models</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/models/new">Add a Model</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/automobiles">Automobiles</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/automobiles/new">Add an Automobile</NavLink>
+                    </li>
+                  </ul>
                 </div>
-              </div>
-              <div className="relative">
-                <button className="bg-green-500 text-black font-semibold py-2 px-4 rounded inline-flex items-center nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                  <span className="px-2">Service</span>
-                </button>
-                <div className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/technicians">
-                        <span className="ml-4 text-base font-medium text-gray-900">Technicians List</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/technicians/new">
-                        <span className="ml-4 text-base font-medium text-gray-900">Create a Technician</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/appointments">
-                        <span className="ml-4 text-base font-medium text-gray-900">Appointments List</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/appointments/new">
-                        <span className="ml-4 text-base font-medium text-gray-900">Create an Appointment</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/appointments/history">
-                        <span className="ml-4 text-base font-medium text-gray-900">Appointments History</span>
-                      </NavLink>
-                    </div>
-                  </div>
+                <button id="dropdownDefaultButton2" data-dropdown-toggle="dropdown2" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Service <svg className="w-4 h-6 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                <div id="dropdown2" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton2">
+                    <li>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/technicians">Technicians List</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/technicians/new">Create a Technician</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/appointments">Appointments List</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/appointments/new">Create an Appointment</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/appointments/history">Appointments History</NavLink>
+                    </li>
+                  </ul>
                 </div>
-              </div>
-              <div className="relative">
-                <button className="bg-green-500 text-black font-semibold py-2 px-4 rounded inline-flex items-center nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                  <span className="px-2">Sales</span>
-                </button>
-                <div className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/salespeople">
-                        <span className="ml-4 text-base font-medium text-gray-900">Salespeople</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/salespeople/new">
-                        <span className="ml-4 text-base font-medium text-gray-900">Add a Salesperson</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/customers">
-                        <span className="ml-4 text-base font-medium text-gray-900">Customers</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/customers/new">
-                        <span className="ml-4 text-base font-medium text-gray-900">Add a Customer</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/sales">
-                        <span className="ml-4 text-base font-medium text-gray-900">Sales</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/sales/new">
-                        <span className="ml-4 text-base font-medium text-gray-900">Add a Sale</span>
-                      </NavLink>
-                      <NavLink className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50" to="/sales/history">
-                        <span className="ml-4 text-base font-medium text-gray-900">Salesperson History</span>
-                      </NavLink>
-                    </div>
-                  </div>
+                <button id="dropdownDefaultButton3" data-dropdown-toggle="dropdown3" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Sales <svg className="w-4 h-6 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                <div id="dropdown3" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton3">
+                    <li>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/salespeople">Salespeople</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/salespeople/new">Add a Salesperson</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/customers">Customers</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/customers/new">Add a Customer</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/sales">Sales</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/sales/new">Add a Sale</NavLink>
+                      <NavLink className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" to="/sales/history">Salesperson History</NavLink>
+                    </li>
+                  </ul>
                 </div>
-              </div>
             </div>
           </div>
         </div>

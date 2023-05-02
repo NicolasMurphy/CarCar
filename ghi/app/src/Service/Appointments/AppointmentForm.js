3b4376ee491414e11cc563b5c7d6c8f1c0b94cb4
Nicolas Murphy <murphynick10@gmail.com>
2023-05-02 14:39:56 -0500

    done styling services

--- ghi/app/src/Service/Appointments/AppointmentForm.js
+++ ghi/app/src/Service/Appointments/AppointmentForm.js
@@ -83,47 +83,74 @@ function AppointmentForm() {
 
 
   return (
-      <div className="row">
-      <div className="offset-3 col-6">
-        <div className="shadow p-4 mt-4">
-          <h1>Create an Appointment</h1>
-          <form onSubmit={handleSubmit} id="create-appointment-form">
+      // <div className="row">
+      // <div className="offset-3 col-6">
+      //   <div className="shadow p-4 mt-4">
+      //     <h1>Create an Appointment</h1>
+      //     <form onSubmit={handleSubmit} id="create-appointment-form">
 
-              <div className="form-floating mb-3">
-                  <input value={vin} onChange={handleVinChange} placeholder="Vin" required type="text" name="vin" id="vin" className="form-control"></input>
-                  <label htmlFor="vin">Vin</label>
-              </div>
-              <div className="form-floating mb-3">
-                  <input value={customer} onChange={handleCustomerChange} placeholder="Customer" required type="text" name="customer" id="customer" className="form-control"></input>
-                  <label htmlFor="customer">Customer</label>
-              </div>
-              <div className="form-floating mb-3">
-                  <input value={date_time} onChange={handleDateTimeChange} placeholder="Datetime" required type="datetime-local" name="date_time" id="date_time" className="form-control"></input>
-                  <label htmlFor="date_time">Date and time</label>
-              </div>
-              <div className="mb-3">
-                <select onChange={handleTechnicianChange} required name="technicians" id="technicians" className="form-select">
-                  <option value="">Choose a technician</option>
-                  {technicians.map(technician => {
-                    return (
-                        <option key={technician.employee_id} value={technician.employee_id}>
-                            {technician.first_name + ' ' + technician.last_name}
-                        </option>
+      //         <div className="form-floating mb-3">
+      //             <input value={vin} onChange={handleVinChange} placeholder="Vin" required type="text" name="vin" id="vin" className="form-control"></input>
+      //             <label htmlFor="vin">Vin</label>
+      //         </div>
+      //         <div className="form-floating mb-3">
+      //             <input value={customer} onChange={handleCustomerChange} placeholder="Customer" required type="text" name="customer" id="customer" className="form-control"></input>
+      //             <label htmlFor="customer">Customer</label>
+      //         </div>
+      //         <div className="form-floating mb-3">
+      //             <input value={date_time} onChange={handleDateTimeChange} placeholder="Datetime" required type="datetime-local" name="date_time" id="date_time" className="form-control"></input>
+      //             <label htmlFor="date_time">Date and time</label>
+      //         </div>
+      //         <div className="mb-3">
+      //           <select onChange={handleTechnicianChange} required name="technicians" id="technicians" className="form-select">
+      //             <option value="">Choose a technician</option>
+      //             {technicians.map(technician => {
+      //               return (
+      //                   <option key={technician.employee_id} value={technician.employee_id}>
+      //                       {technician.first_name + ' ' + technician.last_name}
+      //                   </option>
+      //               );
+      //           })}
+      //           </select>
+      //         </div>
+      //         <div className="form-floating mb-3">
+      //             <input value={reason} onChange={handleReasonChange} placeholder="Reason" required type="text" name="reason" id="reason" className="form-control"></input>
+      //             <label htmlFor="reason">Reason</label>
+      //         </div>
+
+
+      //       <button className="btn btn-primary">Create</button>
+      //     </form>
+      //   </div>
+      // </div>
+      // </div>
+
+<div className="h-screen">
+    <div className="flex flex-col items-center">
+    <br></br>
+    <h2 className="text-4xl font-bold text-white">Create an Appointment</h2>
+    <br></br>
+        <form onSubmit={handleSubmit} className="flex flex-col flex-center bg-gray-800 rounded shadow-lg p-8" action="">
+            <input value={vin} onChange={handleVinChange} className="mb-2 text-white flex items-center h-12 px-4 w-64 bg-gray-600 mt-2 rounded focus:outline-none focus:ring-2" type="text" placeholder="Vin" required/>
+            <input value={customer} onChange={handleCustomerChange} className="mb-2 text-white flex items-center h-12 px-4 w-64 bg-gray-600 mt-2 rounded focus:outline-none focus:ring-2" type="text" placeholder="Customer" required/>
+            <input value={date_time} onChange={handleDateTimeChange} className="mb-2 text-white flex items-center h-12 px-4 w-64 bg-gray-600 mt-2 rounded focus:outline-none focus:ring-2" type="datetime-local" placeholder="Date and Time" required/>
+
+            <select onChange={handleTechnicianChange} className="text-white mb-2 flex items-center h-12 px-4 w-64 bg-gray-600 mt-2 rounded focus:outline-none focus:ring-2">
+                <option>Choose a technican</option>
+                    {technicians.map(technician => {
+                        return (
+                            <option className="text-white" key={technician.employee_id} value={technician.employee_id}>
+                                {technician.first_name + ' ' + technician.last_name}
+                            </option>
                     );
                 })}
-                </select>
-              </div>
-              <div className="form-floating mb-3">
-                  <input value={reason} onChange={handleReasonChange} placeholder="Reason" required type="text" name="reason" id="reason" className="form-control"></input>
-                  <label htmlFor="reason">Reason</label>
-              </div>
+            </select>
 
-
-            <button className="btn btn-primary">Create</button>
-          </form>
-        </div>
-      </div>
-      </div>
+            <input value={reason} onChange={handleReasonChange} className="mb-2 text-white flex items-center h-12 px-4 w-64 bg-gray-600 mt-2 rounded focus:outline-none focus:ring-2" type="text" placeholder="Reason" required/>
+            <button className="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">Create</button>
+        </form>
+    </div>
+</div>
 
   );
 }
--- ghi/app/src/Service/Appointments/AppointmentsList.js
+++ ghi/app/src/Service/Appointments/AppointmentsList.js
@@ -63,7 +63,7 @@ function AppointmentsList() {
 
     return (
       <>
-      <br></br>
+      {/* <br></br>
       <h1>Appointments</h1>
       <table className="table table-striped">
         <thead>
@@ -98,7 +98,50 @@ function AppointmentsList() {
             );
           })}
         </tbody>
-      </table>
+      </table> */}
+
+<div className="h-screen">
+        <br></br>
+            <h2 className="text-4xl font-bold text-white">Appointments</h2>
+        <br></br>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-400">
+                <thead className="text-xs uppercase bg-gray-700 text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">Vin</th>
+                        <th scope="col" className="px-6 py-3">Is VIP?</th>
+                        <th scope="col" className="px-6 py-3">Customer</th>
+                        <th scope="col" className="px-6 py-3">Date</th>
+                        <th scope="col" className="px-6 py-3">Time</th>
+                        <th scope="col" className="px-6 py-3">Technician</th>
+                        <th scope="col" className="px-6 py-3">Reason</th>
+                        <th scope="col" className="px-6 py-3"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {appointments.map(appointment => {
+                      let str = JSON.stringify(autos);
+                        return(
+                            <tr key={appointment.id} className="border-b even:bg-gray-800 odd:bg-gray-900 border-gray-700 ">
+                                <td className="px-6 py-4">{ appointment.vin }</td>
+                                <td className="px-6 py-4">{ ((str.includes(appointment.vin)) ? "Yes" : "No") }</td>
+                                <td className="px-6 py-4">{ appointment.customer }</td>
+                                <td className="px-6 py-4">{ appointment.date_time.split("T")[0] }</td>
+                                <td className="px-6 py-4">{ formatTime(appointment.date_time.split("T")[1].split("+")[0]) }</td>
+                                <td className="px-6 py-4">{ `${appointment.technician.first_name} ${appointment.technician.last_name}` }</td>
+                                <td className="px-6 py-4">{ appointment.reason }</td>
+                                <td className="px-6 py-4">
+                                  <button className="border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700" onClick={handleCancel} id={appointment.id}>Cancel</button>
+                                  <button className="focus:outline-none text-white focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-green-600 hover:bg-green-700 dark:focus:ring-green-800" onClick={handleFinish} id={appointment.id}>Finish</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    </div>
+
       </>
     );
   }
